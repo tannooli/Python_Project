169a58ea70609bed922a719ad056062cb873f796
Oliver Tanno <tannooli@students.zhaw.ch>
2023-04-02 02:57:40 +0200

    Changes for the interface in app, script and index

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
-from flask import Flask, request
+from flask import Flask
+from flask import request
+from flask import jsonify
+from flask.helpers import send_file
+from flask.helpers import send_from_directory
+import torch
+import numpy as np
 
 
 app = Flask(__name__, static_url_path='/', static_folder='PYTHON_PROJEKT')
@@ -7,29 +13,6 @@ app = Flask(__name__, static_url_path='/', static_folder='PYTHON_PROJEKT')
 def hello_world():
     return "Hello Python Projekt!"
 
-@app.route("/sentiment", methods=["GET"])
-def test():
-text = request.args.get('text')
-if (text == ""):
-    return "Please use text parameter in GET URL"
-#... do service calculation, run inference
-return result
-
-
-# Username
-@app.route('/user/<username>')
-def show_user_profile(username):
-# show the user profile for that user
-return f'User {escape(username)}'
-
-#Post
-@app.route('/post/<int:post_id>')
-def show_post(post_id):
-# show the post with the given id, the id is an integer
-return f'Post {post_id}'
-
-#Subpath
-@app.route('/path/<path:subpath>')
-def show_subpath(subpath):
-# show the subpath after /path/
-return f'Subpath {escape(subpath)}'
+@app.route("/")
+def indexPage():
+return send_file("frontend/index.html")
--- script.js
+++ script.js
@@ -1,25 +1,33 @@
-function getSentiment(event, text) {
-  if (!text || event.key !== "Enter") {
-    answer.innerHTML = '';
-    return;
+// Preview
+if (file) {
+  preview.src = URL.createObjectURL(files[0])
   }
-  answerPart.style.visibility = "visible";
-  // Get Sentiment
-  fetch('/sentiment?' + new URLSearchParams({
-    text: text,
-  }), {
-    method: 'GET',
-    headers: {}
-  }).then(
-    response => {
-      console.log(response)
-      response.text().then(function (text) {
-        answer.innerHTML = text;
-      });
-    }
-  ).then(
-    success => console.log(success)
-  ).catch(
-    error => console.log(error)
-  );
-}
+  
+//Interface
+const formular = document.getElementById("image-input");
+const bildEingabe = document.getElementById("image-upload");
+
+formular.addEventListener("submit", async function (event) {
+  event.preventDefault();
+
+  const formData = new FormData();
+  formData.append("bild", bildEingabe.files[0]);
+
+  const response = await fetch(
+    "https://api-inference.huggingface.co/models/facebook/maskformer-swin-large-ade",
+    {
+      headers: { Authorization: "Bearer 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" },
+      method: "POST",
+      body: formData
+    }).then(response => response.json())
+    .then(data => {
+      console.log(data);
+      const resultElement = document.getElementById('result');
+      resultElement.innerText = JSON.stringify(data);
+    })
+    .catch(error => console.error(error));
+})
+
+
+
+
