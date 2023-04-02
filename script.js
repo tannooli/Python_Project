4e62f211dc30d72c546aa2526ba9e4dbed825b5d
Oliver Tanno <tannooli@students.zhaw.ch>
2023-04-02 17:33:49 +0200

    Vanilla.js integration

--- script.js
+++ script.js
@@ -3,23 +3,24 @@
 const formular = document.getElementById("image-input");
 const bildEingabe = document.getElementById("image-upload");
 
-formular.addEventListener("submit", async function (event) {
+formular.addEventListener("submit", function (event) {
   event.preventDefault();
 
   const formData = new FormData();
   formData.append("bild", bildEingabe.files[0]);
 
-  const response = await fetch(
+  fetch(
     "https://api-inference.huggingface.co/models/facebook/maskformer-swin-large-ade",
     {
       headers: { Authorization: "Bearer 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" },
       method: "POST",
       body: formData
-    }).then(response => response.json())
+    })
+    .then(response => response.json())
     .then(data => {
       console.log(data);
       const resultElement = document.getElementById('result');
       resultElement.innerText = JSON.stringify(data);
     })
     .catch(error => console.error(error));
-})
+});
